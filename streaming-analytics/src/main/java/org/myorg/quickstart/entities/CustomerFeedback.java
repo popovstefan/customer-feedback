0206f73882d62da67fe78c0d695a696530b0a365
Stefan Popov <steffan.popov@protonmail.com>
2024-11-24 15:39:29 +0100

    Add Lombok dependency, annotate source entities

--- streaming-analytics/src/main/java/org/myorg/quickstart/entities/CustomerFeedback.java
+++ streaming-analytics/src/main/java/org/myorg/quickstart/entities/CustomerFeedback.java
@@ -1,4 +1,24 @@
 package org.myorg.quickstart.entities;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"customerId", "purchaseId"})
 public class CustomerFeedback {
+    private String customerId;
+    private String purchaseId;
+    private long dateOfBirth;
+    private long timestamp;
+    private String gender;
+    private String country;
+    private int productQuality;
+    private int serviceQuality;
+    private String feedbackScore;
+    private String loyaltyLevel;
+    private int customerIncome;
 }
--- streaming-analytics/src/main/java/org/myorg/quickstart/entities/Purchase.java
+++ streaming-analytics/src/main/java/org/myorg/quickstart/entities/Purchase.java
@@ -1,4 +1,18 @@
 package org.myorg.quickstart.entities;
 
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"purchaseId", "customerId", "productId"})
 public class Purchase {
+    private String purchaseId;
+    private String customerId;
+    private String productId;
+    private long timestamp;
+    private double unitAmount;
+    private int numUnits;
 }
